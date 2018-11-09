bb5d77de52ef36f9bf4b913899553f7b3158084c
Erwin van der Valk <erwin@vandervalk.pro>
2018-11-09 12:22:42 +0100

    Add example application and extend readme

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # Templated Configuration Source for Microsoft.Extensions.Configuration
+
+This library is an extension for Microsoft.Extensions.Configuration, which helps you to do recursive templating in configuration settings. 
+
+When retrieving a configuration setting value, it will look for {placeholders}. If it finds any placeholder, it will try to see if the placeholder is actually configured as a setting. It will then (recursively) replace any placeholders within the setting's value. 
+
+This means you can compose configuration settings out of other configuration settings, which will allow you to override only parts of a config setting or the entire setting.
+
+## Getting Started
+
+* Add a reference to TemplatedConfiguration
+* Add a using statement to 'TemplatedConfiguration'
+
+* Wrap the normal 'configuration builder' with a templated support builder, preferably by using the 
+**WithRecursiveTemplateSupport** Extension method.
+
+``` c#
+
+    using TemplatedConfiguration;
+
+    /* ... */
+
+        var config = new ConfigurationBuilder()
+            .WithRecursiveTemplateSupport(
+                // Wrap the configuration providers with the provider that supports templating
+                builder => builder
+                    // First set up defaults
+                    // Then set up the 'overrides'. 
+                    .AddIniFile("Config.ini")
+                    .AddCommandLine(args)
+                    .AddEnvironmentVariables()
+                )
+            .Build();
+```
+## Contributing
+
+## Licencing
+
+
+
+
---
+++ src/TemplatedConfiguration.Example/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+
+namespace TemplatedConfiguration.Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .WithRecursiveTemplateSupport(
+
+                    // Wrap the configuration providers with the provider that supports templating
+                    builder => builder
+                        // First set up defaults
+                        .AddInMemoryCollection(Global.DefaultSettings)
+                        .AddInMemoryCollection(ComponentA.DefaultSettings)
+                        .AddInMemoryCollection(ComponentB.DefaultSettings)
+
+                        // Then set up the 'overrides'. 
+                        .AddIniFile("Config.ini")
+                        .AddCommandLine(args)
+                        .AddEnvironmentVariables()
+                        
+                    )
+                .Build();
+
+            Console.WriteLine("Component A: Connection String: " +config.GetValue<string>("ComponentA.ConnectionString"));
+            Console.WriteLine("Component B: Connection String: " +config.GetValue<string>("ComponentB.ConnectionString"));
+
+            Console.ReadLine();
+        }
+
+        public class Global
+        {
+            public static readonly Dictionary<string, string> DefaultSettings = new Dictionary<string, string>()
+            {
+                // Default global settings. 
+                {"Global.ConnectionString.Security", "Integrated Security=true;" },
+                {"ComponentA.ConnectionString.Settings", "" },
+            };
+        }
+
+        public class ComponentA
+        {
+            public static readonly Dictionary<string, string> DefaultSettings = new Dictionary<string, string>()
+            {
+                // The connection string is composed from parts
+                // But can also be overwritten as a whole
+                {"ComponentA.ConnectionString", "{ComponentA.ConnectionString.Security}Server={ComponentA.ConnectionString.ServerName};Database={ComponentA.ConnectionString.DatabaseName}{ComponentA.ConnectionString.Settings}"},
+
+                // Each part can be overwritten per 'component' or globally
+                {"ComponentA.ConnectionString.Security", "{Global.ConnectionString.Security}" },
+                {"ComponentA.ConnectionString.ServerName", "{Global.ConnectionString.ServerName}" },
+                {"ComponentA.ConnectionString.Settings", "{Global.ConnectionString.Settings}" },
+
+                // Some default settings are simply hard coded, such as the default database name
+                {"ComponentA.ConnectionString.DatabaseName", "ComponentA" },
+            };
+        }
+        public class ComponentB
+        {
+            public static readonly Dictionary<string, string> DefaultSettings = new Dictionary<string, string>()
+            {
+                // The connection string is composed from parts
+                // But can also be overwritten as a whole
+                {"ComponentB.ConnectionString", "{ComponentB.ConnectionString.Security}Server={ComponentB.ServerName};Database={ComponentB.DatabaseName}{ComponentB.ConnectionString.Settings}"},
+
+                // Each part can be overwritten per 'component' or globally
+                {"ComponentB.ConnectionString.Security", "{Global.ConnectionString.Security}" },
+                {"ComponentB.ConnectionString.ServerName", "{Global.ConnectionString.ServerName}" },
+                {"ComponentB.ConnectionString.Settings", "{Global.ConnectionString.Settings}" },
+
+                // Some default settings are simply hard coded, such as the default database name
+                {"ComponentB.ConnectionString.DatabaseName", "ComponentA" },
+            };
+        }
+    }
+}
